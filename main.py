82a19f92202e43adf1469fe18e24d2e39c3b5aa5
ireallyhateyou <muslimgauzer420@proton.me>
2025-11-20 15:31:11 +0100

    load data from NASA/ESA + fix curses stuff

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ from skyfield.api import Star, load, wgs84
 from skyfield.data import hipparcos
 from skyfield.projections import build_stereographic_projection
 
-# configuration for ASCII art
-h = 24, w = 80
-fov = 10 # degrees
+# configuration for ASCII art and camera
+h = 24
+w = 80
+azimuth = 180.0 # all in degrees
+alt = 30.0 
+fov = 10.0 
 scale = " .,:;+*#@"
 
 # configuration for spaceslop
@@ -22,13 +25,33 @@ def main(stdscr):
     curses.curs_set(0)
     stdscr.nodelay(1)
     stdscr.timeout(50)
-    ts = load.timescale()
+    sh, sw = stdscr.getmaxyx() # fetch terminal size
+    h = sh 
+    w = sw 
+    stdscr.addstr(h//2, w//2 - 26, "downloading planet data...")
+    stdscr.refresh()
+
     # jpl ephemeris - https://ssd.jpl.nasa.gov/ephem.html
+    ts = load.timescale()
     planets = load('de421.bsp')
     earth = planets['earth']
-    stdscr.addstr(h//2, w//2 - 10, "downloading star data...")
-    stdscr.refresh()
     observer = earth + wgs84.latlon(lat, long)
+    bodies = { # celestial bodies that we will track
+        'Mars': planets['mars'],
+        'Venus': planets['venus'],
+        'Jup': planets['jupiter barycenter'],
+        'Sat': planets['saturn barycenter'],
+        'Moon': planets['moon']
+    }
+
+    # star data from hipparicos - https://www.cosmos.esa.int/web/hipparcos/
+    stdscr.clear()
+    stdscr.addstr(h//2, w//2 - 24, "downloading star data...")
+    stdscr.refresh()
+    with load.open(hipparcos.URL) as f:
+        df = hipparcos.load_dataframe(f)
+    bright_stars = df[df['magnitude'] <= 3.5]
+    stars = Star.from_dataframe(bright_stars)
 
 if __name__ == "__main__":
     curses.wrapper(main)
